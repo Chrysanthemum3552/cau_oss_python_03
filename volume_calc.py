476941ca6579d894d0ae8eb02f526577e2274480
Jacob <chrysanthemum3552@gmail.com>
2023-04-04 15:23:04 +0900

    I add Hello world code and hey code

--- volume_calc.py
+++ volume_calc.py
@@ -4,3 +4,7 @@ depth=float(input("depth:")) #높이 길이를 입력합니다.
 
 volume=width*height*depth #입력 받은 값을 토대로 부피를 계산합니다.
 print("The desired volume is",volume) #계산한 부피 값을 출력합니다.
+
+print("Hello World!")
+
+print("Hey Hey") #Hey Hey
